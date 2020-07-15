06d55518ccf4744cd0ee86308da021c4c494c001
Stefan <stefan0@stefan0.com>
2020-07-15 14:09:08 -0700

    More model tests.

--- fruit_app/tests/test_models.py
+++ fruit_app/tests/test_models.py
@@ -4,8 +4,32 @@ from django.db.models import Q
 import decimal
 
 
+class Country_Test(TestCase):
+    """ Test module for Country model """
+
+    def test_country_code_converted_to_upper_case(self):
+        Country.objects.create(
+            country_code='mx',
+            name='Mexico'
+        )
+        mexico = Country.objects.get(country_code='MX')
+        if mexico is None:
+            self.assertEqual(True, False)
+        self.assertEqual('MX', mexico.country_code)
+
+    def test_country_code_is_2letters(self):
+        try:
+            Country.objects.create(
+                country_code='mex',
+                name='Mexico'
+            )
+            self.assertEqual(True, False)
+        except:
+            self.assertEqual(True, True)
+
+
 class CommodityData_Test(TestCase):
-    """ Test module for Puppy model """
+    """ Test module for CommodityData model """
 
     def setUp(self):
         Country.objects.create(
@@ -34,13 +58,81 @@ class CommodityData_Test(TestCase):
         # print('setUp')
         # print(list(qs))
 
+    def test_unique(self):
+        mexico = Country.objects.get(country_code='MX')
+        try:
+            CommodityData.objects.create(
+                country=mexico,
+                commodity='mango',
+                fixed_overhead=decimal.Decimal(20.00),
+                variable_cost=decimal.Decimal(1.00)
+            )
+            self.assertEqual(True, False)
+        except:
+            self.assertEqual(True, True)
+
+    def test_country_required(self):
+        try:
+            CommodityData.objects.create(
+                country='MX',
+                commodity='mango',
+                fixed_overhead=decimal.Decimal(20.00),
+                variable_cost=decimal.Decimal(1.00)
+            )
+            self.assertEqual(True, False)
+        except:
+            self.assertEqual(True, True)
+
+    def test_costs_must_be_decimal8x2(self):
+        mexico = Country.objects.get(country_code='MX')
+        try:
+            CommodityData.objects.create(
+                country=mexico,
+                commodity='apple',
+                fixed_overhead=decimal.Decimal(20.00),
+                variable_cost=decimal.Decimal(1.00)
+            )
+            self.assertEqual(True, True)
+        except:
+            self.assertEqual(True, False)
+        try:
+            CommodityData.objects.create(
+                country=mexico,
+                commodity='apple',
+                fixed_overhead=decimal.Decimal(20.999),
+                variable_cost=decimal.Decimal(1.00)
+            )
+            self.assertEqual(True, False)
+        except:
+            self.assertEqual(True, True)
+        try:
+            CommodityData.objects.create(
+                country=mexico,
+                commodity='apple',
+                fixed_overhead=decimal.Decimal(20.99),
+                variable_cost=decimal.Decimal(1.001)
+            )
+            self.assertEqual(True, False)
+        except:
+            self.assertEqual(True, True)
+
+    def test_commodity_required(self):
+        try:
+            CommodityData.objects.create(
+                country='MX',
+                commodity='',
+                fixed_overhead=decimal.Decimal(20.00),
+                variable_cost=decimal.Decimal(1.00)
+            )
+            self.assertEqual(True, False)
+        except:
+            self.assertEqual(True, True)
+
     def test_composite_key(self):
         mexico = Country.objects.get(country_code='MX')
         qs = CommodityData.objects.filter(
             Q(country=mexico) & Q(commodity='mango')
         )
-        # print(list(qs))
-        count = qs.count()
         self.assertEqual(
             qs.count(), 1
         )
